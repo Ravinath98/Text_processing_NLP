4d2e60ea9b4de5c2f1df69d2a1c86332833ed36a
Ravinath98 <62192952+Ravinath98@users.noreply.github.com>
2021-06-30 19:16:45 +0530

    Add files via upload

--- Lemmatization.py
+++ Lemmatization.py
@@ -1,21 +1,15 @@
-#import the existing word and sentence tokenizing
-#libraries
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk import word_tokenize, pos_tag
+from collections import defaultdict
+tag_map = defaultdict(lambda : wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
 
-text = "Natural language processing (NLP) is a field "	+ \
-"of computer science, artificial intelligence " + \
-"and computational linguistics concerned with "	+ \
-"the interactions between computers and human "	+ \
-"(natural) languages, and, in particular, "	+ \
-"concerned with programming computers to "	+ \
-"fruitfully process large natural language " + \
-"corpora. Challenges in natural language " + \
-"processing frequently involve natural " + \
-"language understanding, natural language" + \
-"generation frequently from formal, machine" + \
-"-readable logical forms), connecting language " + \
-"and machine perception, managing human-" +\
-"computer dialog systems, or some combination "	+ \
-"thereof."
-print(sent_tokenize(text))
-print(word_tokenize(text))
+text = "guru99 is a totally new kind of learning experience."
+tokens = word_tokenize(text)
+lemma_function = WordNetLemmatizer()
+for token,tag in pos_tag(tokens):
+    lemma = lemma_function.lemmatize(token, tag_map[tag[0]])
+    print(token, "=>", lemma)
